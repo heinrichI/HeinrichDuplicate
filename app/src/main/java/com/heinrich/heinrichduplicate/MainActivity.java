94a0d2f6b5f3369514539d0068b7d830bf34fe1a
Heinrich <jeronimo73@gmail.com>
2018-05-25 17:25:11 +0300

    1
    Signed-off-by: Heinrich <jeronimo73@gmail.com>

--- app/src/main/java/com/heinrich/heinrichduplicate/MainActivity.java
+++ app/src/main/java/com/heinrich/heinrichduplicate/MainActivity.java
@@ -1,6 +1,7 @@
 package com.heinrich.heinrichduplicate;
 
 import android.app.Activity;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
@@ -14,15 +15,16 @@ import android.widget.ListView;
 
 import com.heinrich.heinrichduplicate.config.API;
 
-import net.rdrei.android.dirchooser.DirectoryChooserActivity;
-import net.rdrei.android.dirchooser.DirectoryChooserConfig;
-
 import java.util.ArrayList;
 import java.util.List;
 
-public class MainActivity extends AppCompatActivity {
+
+import lib.folderpicker.FolderPicker;
+
+public class MainActivity extends Activity {
 
     private static final int REQUEST_DIRECTORY = 2;
+    private static final int FOLDERPICKER_CODE = 3;
 
     private List<String> _foldersPath = new ArrayList<String>();
     ArrayAdapter<String> _adapter;
@@ -33,6 +35,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        PermissionsHelper.getReadStoragePermissions(_act);
+
         ListView lvFolders = (ListView) findViewById(R.id.folders);
         _adapter = new ArrayAdapter<String>(this,
                 android.R.layout.simple_list_item_1,
@@ -75,7 +80,7 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void selectFolder() {
-        final Intent chooserIntent = new Intent(
+        /*final Intent chooserIntent = new Intent(
                 this,
                 DirectoryChooserActivity.class);
 
@@ -90,19 +95,41 @@ public class MainActivity extends AppCompatActivity {
                 DirectoryChooserActivity.EXTRA_CONFIG,
                 config);
 
-        startActivityForResult(chooserIntent, REQUEST_DIRECTORY);
+        startActivityForResult(chooserIntent, REQUEST_DIRECTORY);*/
+
+        /*if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
+            Intent i = new Intent(Intent.ACTION_OPEN_DOCUMENT_TREE);
+            i.addCategory(Intent.CATEGORY_BROWSABLE);
+            startActivityForResult(Intent.createChooser(i, "Choose directory"), 9999);
+        }*/
+
+        Intent intent = new Intent(this, FolderPicker.class);
+        startActivityForResult(intent, FOLDERPICKER_CODE);
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-        if (requestCode == REQUEST_DIRECTORY) {
-            if (resultCode == DirectoryChooserActivity.RESULT_CODE_DIR_SELECTED) {
-                String dir = data.getStringExtra(DirectoryChooserActivity.RESULT_SELECTED_DIR);
-                Log.d("onActivityResult", dir);
-               _foldersPath.add(dir);
-                _adapter.notifyDataSetChanged();
-            }
+        Log.d("onActivityResult:", "onActivityResult");
+        switch(requestCode) {
+//            case REQUEST_DIRECTORY:
+//                if (resultCode == DirectoryChooserActivity.RESULT_CODE_DIR_SELECTED) {
+//                    String dir = data.getStringExtra(DirectoryChooserActivity.RESULT_SELECTED_DIR);
+//                    Log.d("onActivityResult", dir);
+//                    _foldersPath.add(dir);
+//                    _adapter.notifyDataSetChanged();
+//                }
+//                break;
+            case 9999:
+                Log.i("Test", "Result URI " + data.getData());
+                break;
+            case FOLDERPICKER_CODE:
+                if (resultCode == Activity.RESULT_OK) {
+                    String folderLocation = data.getExtras().getString("data");
+                    Log.i("folderLocation", folderLocation);
+                }
+
+                break;
         }
     }
 }
